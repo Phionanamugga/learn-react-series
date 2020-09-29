9aacc3bf779cd7c1db8e375237a63d9661a6459c
ericmuh <muhwezi1000@gmail.com>
2020-09-29 13:04:12 +0300

    Begin Blog app in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import Jumbtron from "./components/jumbtron/Jumbtron";
+import Ideas from "./components/ideas/Ideas";
+
+// importing styles 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <Navbar />
+      <Header />
+      <Jumbtron />
+      <Ideas />
     </div>
   );
 }
---
+++ src/components/jumbtron/Jumbtron.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Jumbtron = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default Jumbtron
